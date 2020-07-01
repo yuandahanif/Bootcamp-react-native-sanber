224c5c5de4aed42fee161dfa27881f0593b81885
yuandahanif <kirishima699@gmail.com>
2020-07-01 13:56:52 +0700

    persiapan tugas_13

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import App from './tugas/Tugas12/App';
+import App from './tugas/Tugas13/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ tugas/Tugas13/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import LoginScreen from './LoginScreen';
+
+export default function App() {
+  return <LoginScreen />;
+}
