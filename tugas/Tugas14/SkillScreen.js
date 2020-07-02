b404606501f1acbfb50494338089c533696fbc7e
yuandahanif <kirishima699@gmail.com>
2020-07-02 21:25:09 +0700

    selesai tugas 1 WIP. halaman beranda aplikasi

--- tugas/Tugas14/SkillScreen.js
+++ tugas/Tugas14/SkillScreen.js
@@ -9,33 +9,15 @@ import {
 import Icon from 'react-native-vector-icons/Feather';
 import TabBar from './components/TabBar';
 import Text from './components/Text';
-import ImageLogo from './components/ImageLogo';
+import SkillList from './components/SkillList';
+import skillData from './skillData.json';
+import dataSociall from './socialData.json';
 
-const dataSociall = [
-  {iName: 'instagram', color: '#E1306C', account: '@yuanda.yuu'},
-  {iName: 'facebook', color: '#4267B2', account: '@yuanda.yuu'},
-  {iName: 'github', color: '#211F1F', account: '@yuandaHanif'},
-  {iName: 'gitlab', color: '#FC6D27', account: '@yuandaHanif'},
-  {iName: 'linkedin', color: '#006192', account: 'Yuanda Hanif Hisyam'},
-];
-const lanuageSkill = [
-  {
-    name: 'HTML5',
-    logoPath: require('./images/html5-original.png'),
-    percent: '85',
-  },
-  {
-    name: 'CSS3',
-    logoPath: require('./images/css3-original.png'),
-    percent: '80',
-  },
-  {
-    name: 'Javascript',
-    logoPath: require('./images/javascript-original.png'),
-    percent: '75',
-  },
-  {name: 'PHP7', logoPath: require('./images/php-original.png'), percent: '75'},
-];
+const languageSkill = skillData.items.filter(v => v.category === 'Language');
+const LibrarySkill = skillData.items.filter(v => v.category === 'Library');
+const TechnologySkill = skillData.items.filter(
+  v => v.category === 'Technology',
+);
 
 export default function AbouteMeScreen() {
   return (
@@ -63,90 +45,31 @@ export default function AbouteMeScreen() {
         <View style={styles.profileInfoSocial}>
           <Text text="Terhubung" size={11} weight="Light" />
           <View style={styles.listSocial}>
-            <TouchableOpacity
-              onPressIn={() => {
-                alert('@yuanda.yuu');
-              }}>
-              <Icon name="instagram" size={24} color="#E1306C" />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPressIn={() => {
-                alert('@yuanda.yuu');
-              }}>
-              <Icon name="facebook" size={24} color="#4267B2" />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPressIn={() => {
-                alert('@yuandaHanif');
-              }}>
-              <Icon name="github" size={24} color="#211F1F" />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPressIn={() => {
-                alert('@yuandaHanif');
-              }}>
-              <Icon name="gitlab" size={24} color="#FC6D27" />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPressIn={() => {
-                alert('@Yuanda Hanif Hisyam');
-              }}>
-              <Icon name="linkedin" size={24} color="#006192" />
-            </TouchableOpacity>
+            <FlatList
+              contentContainerStyle={styles.listSocial}
+              data={dataSociall}
+              renderItem={data => (
+                <TouchableOpacity
+                  onPressIn={() => {
+                    alert(data.item.account);
+                  }}>
+                  <Icon
+                    name={data.item.iName}
+                    size={24}
+                    color={data.item.color}
+                  />
+                </TouchableOpacity>
+              )}
+              keyExtractor={data => data.iName}
+              horizontal={true}
+            />
           </View>
         </View>
       </View>
       <View style={styles.profileSkill}>
-        <TouchableOpacity style={styles.editAkun} activeOpacity={0.8}>
-          <Icon name="settings" size={11} style={styles.settingIcon} />
-          <Text text="Edit akun" />
-        </TouchableOpacity>
-
-        <Text text="Bahasa Pemrograman" size={14} />
-        <FlatList
-          data={lanuageSkill}
-          renderItem={data => (
-            <View style={styles.skillInfo}>
-              <ImageLogo file={data.item.logoPath} size={28} />
-              <View style={styles.skillInfoDetail}>
-                <View style={styles.skillInfoTop}>
-                  <Text
-                    text={data.item.name}
-                    weight="Regular"
-                    badge={true}
-                    badgeText="Advanced"
-                    badgeColor="#E1306C"
-                    badgeTextColor="white"
-                  />
-                  <Text text="85%" weight="Regular" />
-                </View>
-                <View style={styles.skillInfoPercent}>
-                  <View style={styles.percent} />
-                </View>
-              </View>
-            </View>
-          )}
-          keyExtractor={data => data.name}
-        />
-        {/* <View style={styles.skillInfo}>
-          <ImageLogo file={require('./images/html5-original.png')} size={28} />
-          <View style={styles.skillInfoDetail}>
-            <View style={styles.skillInfoTop}>
-              <Text
-                text="HTML5"
-                weight="Regular"
-                badge={true}
-                badgeText="Advanced"
-                badgeColor="#E1306C"
-                badgeTextColor="white"
-              />
-              <Text text="85%" weight="Regular" />
-            </View>
-            <View style={styles.skillInfoPercent}>
-              <View style={styles.percent} />
-            </View>
-          </View>
-        </View> */}
+        <SkillList title="Skill Bahasa Pemrograman" skillData={languageSkill} />
+        <SkillList title="Framework / Library" skillData={LibrarySkill} />
+        <SkillList title="Teknologi" skillData={TechnologySkill} />
       </View>
       <TabBar />
     </View>
@@ -206,31 +129,9 @@ const styles = StyleSheet.create({
   profileSkill: {
     flex: 10,
     marginHorizontal: 15,
+    paddingTop: 15,
   },
-  skillInfo: {
-    marginTop: 10,
+  skillCategoryContainer: {
     flexDirection: 'row',
-  },
-  skillInfoDetail: {
-    flex: 1,
-    marginLeft: 5,
-  },
-  skillInfoTop: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  skillInfoPercent: {
-    height: 5,
-    width: '100%',
-    backgroundColor: '#C4C4C4',
-    borderRadius: 4,
-    marginTop: 5,
-  },
-  percent: {
-    height: '100%',
-    width: '85%',
-    borderRadius: 4,
-    backgroundColor: '#83CD29',
   },
 });
