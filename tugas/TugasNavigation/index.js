72900dd49ea213ee43520770f33914615e193c84
yuandahanif <kirishima699@gmail.com>
2020-07-04 00:20:06 +0700

    selesai soal 2 tugas_15 login, logout, navigasi

---
+++ tugas/TugasNavigation/index.js
@@ -0,0 +1,135 @@
+import React, {useState, useMemo} from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
+import {
+  AbouteMeScreen,
+  AddProjectScreen,
+  LoginScreen,
+  SkillScreen,
+  ProjectScreen,
+  SearchScreen,
+} from './screen/index';
+import TabBar from './components/TabBar';
+import {AuthContext} from './context';
+
+// * screen
+const SkillStack = createStackNavigator();
+const SkillStackScreen = () => (
+  <SkillStack.Navigator>
+    <SkillStack.Screen name="Home" component={SkillScreen} />
+  </SkillStack.Navigator>
+);
+
+const AboutMeStack = createStackNavigator();
+const AboutMeStackScreen = () => (
+  <AboutMeStack.Navigator>
+    <AboutMeStack.Screen name="AbouteMe" component={AbouteMeScreen} />
+  </AboutMeStack.Navigator>
+);
+
+const AddProjectStack = createStackNavigator();
+const AddProjectStackScreen = () => (
+  <AddProjectStack.Navigator>
+    <AddProjectStack.Screen name="Add" component={AddProjectScreen} />
+  </AddProjectStack.Navigator>
+);
+
+const LoginStack = createStackNavigator();
+const LoginStackScreen = () => (
+  <LoginStack.Navigator>
+    <LoginStack.Screen name="Login" component={LoginScreen} />
+  </LoginStack.Navigator>
+);
+
+const ProjectStack = createStackNavigator();
+const ProjectStackScreen = () => (
+  <ProjectStack.Navigator>
+    <ProjectStack.Screen name="Project" component={ProjectScreen} />
+  </ProjectStack.Navigator>
+);
+const SearchStack = createStackNavigator();
+const SearchStackScreen = () => (
+  <SearchStack.Navigator>
+    <SearchStack.Screen name="Search" component={SearchScreen} />
+  </SearchStack.Navigator>
+);
+
+// * Tab
+const Tabs = createBottomTabNavigator();
+const TabsScreen = () => (
+  <Tabs.Navigator
+    initialRouteName="Home"
+    headerMode="none"
+    tabBar={props => <TabBar {...props} />}>
+    <Tabs.Screen
+      name="Home"
+      component={SkillStackScreen}
+      options={{icon: 'home'}}
+    />
+    <Tabs.Screen
+      name="Search"
+      component={SearchStackScreen}
+      options={{icon: 'search'}}
+    />
+    <Tabs.Screen
+      name="add"
+      component={AddProjectStackScreen}
+      options={{isAddButton: true}}
+    />
+    <Tabs.Screen
+      name="Notification"
+      component={ProjectStackScreen}
+      options={{icon: 'bell'}}
+    />
+    <Tabs.Screen
+      name="AbouteMe"
+      component={AboutMeStackScreen}
+      options={{icon: 'user'}}
+    />
+  </Tabs.Navigator>
+);
+
+const DrawerStack = createDrawerNavigator();
+const DrawerScreen = () => (
+  <DrawerStack.Navigator>
+    <DrawerStack.Screen name="Home" component={TabsScreen} />
+    <DrawerStack.Screen name="AbouteMe" component={AboutMeStackScreen} />
+  </DrawerStack.Navigator>
+);
+
+export default function index() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [authToken, setAuth] = useState(null);
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const authContext = useMemo(
+    () => ({
+      signIn: () => setAuth('asdasd'),
+      signOut: () => setAuth(null),
+    }),
+    [],
+  );
+
+  return (
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        {authToken ? (
+          <DrawerStack.Navigator>
+            <DrawerStack.Screen name="Home" component={TabsScreen} />
+            <DrawerStack.Screen
+              name="AbouteMe"
+              component={AboutMeStackScreen}
+            />
+          </DrawerStack.Navigator>
+        ) : (
+          <LoginStack.Navigator>
+            <LoginStack.Screen name="Login" component={LoginScreen} />
+          </LoginStack.Navigator>
+        )}
+      </NavigationContainer>
+    </AuthContext.Provider>
+  );
+}
---
+++ tugas/TugasNavigation/screen/index.js
@@ -0,0 +1,14 @@
+import AbouteMeScreen from './AbouteMeScreen';
+import AddProjectScreen from './AddScreen';
+import LoginScreen from './LoginScreen';
+import ProjectScreen from './ProjectScreen';
+import SkillScreen from './SkillScreen';
+import SearchScreen from './SearchScreen';
+export {
+  AbouteMeScreen,
+  AddProjectScreen,
+  LoginScreen,
+  ProjectScreen,
+  SkillScreen,
+  SearchScreen
+};
